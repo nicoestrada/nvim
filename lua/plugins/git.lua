af5b1c9741cee4e5bdcdd3152af34bd03dfb2c72
Nico <estrada@hey.com>
2025-04-05 11:05:40 -0500

    fixed remap bw gitsigns/push

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,36 +5,36 @@ return {
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
             vim.keymap.set("n", "<leader>ga", function()
-                vim.cmd("Git add .")
-            end,
-            {
-                noremap = true,
-            });
+                    vim.cmd("Git add .")
+                end,
+                {
+                    noremap = true,
+                });
 
 
             vim.keymap.set("n", "<leader>gc", function()
-                local message = vim.fn.input("Commit message: ")
-                if message ~= "" then
-                    vim.cmd('Git commit -m "' .. message .. '"')
-                end
-            end,
-            {
-                noremap = true,
-            });
+                    local message = vim.fn.input("Commit message: ")
+                    if message ~= "" then
+                        vim.cmd('Git commit -m "' .. message .. '"')
+                    end
+                end,
+                {
+                    noremap = true,
+                });
 
             vim.keymap.set("n", "<leader>gp", function()
-                vim.cmd("Git push origin master")
-            end,
-            {
-                noremap = true,
-            });    
+                    vim.cmd("Git push origin master")
+                end,
+                {
+                    noremap = true,
+                });
         end,
-        },
-        {
-            "lewis6991/gitsigns.nvim",
-            config = function()
-                require("gitsigns").setup()
-                vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-            end,
-        },
-    }
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+        end,
+    },
+}
